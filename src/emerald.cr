f9f60463fbcb4c5392a7d6a13555262dbd8834eb
LordNature <lordnature.christian@gmail.com>
2017-08-29 08:29:22 -0500

    SQLite3 Database

--- src/emerald.cr
+++ src/emerald.cr
@@ -1,9 +1,27 @@
+require "markdown"
 require "kemal"
 require "./emerald/*"
 
 module Emerald
-	get "/" do
-		"Hello!"
+	get "/" do |env|
+		Emerald::DB.db.query("SELECT * FROM posts") do |rs|
+			rs.each do
+				id = rs.read(Int64)
+				title = rs.read(String)
+				author = rs.read(String)
+				hook = rs.read(String)
+				body = rs.read(String)
+				date = rs.read(Int)
+
+
+				env.response.puts("#{id} #{title} #{author} #{hook} #{body} #{date}")
+			end
+		end
+	end
+
+	get "/blog/:id" do |env|
+		id = env.params.url["id"]
+		render "src/views/blog.ecr"
 	end
 end
 
