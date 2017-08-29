f1dbdc9ecd50555da50887e8edb1af976d777f59
LordNature <lordnature.christian@gmail.com>
2017-08-29 13:45:48 -0500

    Added layout, blog page, and blog/id/

--- src/emerald.cr
+++ src/emerald.cr
@@ -4,24 +4,12 @@ require "./emerald/*"
 
 module Emerald
 	get "/" do |env|
-		Emerald::DB.db.query("SELECT * FROM posts") do |rs|
-			rs.each do
-				id = rs.read(Int64)
-				title = rs.read(String)
-				author = rs.read(String)
-				hook = rs.read(String)
-				body = rs.read(String)
-				date = rs.read(Int)
-
-
-				env.response.puts("#{id} #{title} #{author} #{hook} #{body} #{date}")
-			end
-		end
+		render "src/views/blog.ecr", "src/views/layout.ecr"
 	end
 
 	get "/blog/:id" do |env|
 		id = env.params.url["id"]
-		render "src/views/blog.ecr"
+		render "src/views/post.ecr", "src/views/layout.ecr"
 	end
 end
 
