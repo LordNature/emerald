4a3355aefc72a337395c01628fc1b6bf132a5f60
LordNature <lordnature.christian@gmail.com>
2018-01-30 08:53:10 -0600

    Changes to goodies

---
+++ src/emerald/login-controller.cr
@@ -0,0 +1,45 @@
+require "remarkdown"
+
+module Emerald
+  class Post
+    ::DB.mapping({
+      id:     Int64,
+      title:  String,
+      author: String,
+      hook:   String,
+      body:   String,
+      date:   Int64,
+    })
+    JSON.mapping({
+      id:     Int64,
+      title:  String,
+      author: String,
+      hook:   String,
+      body:   String,
+      date:   Int64,
+    })
+
+    def self.query(id)
+      title, author, hook, body, date = Emerald::DB.db.query_one "SELECT title, author, hook, body, date FROM posts WHERE id = #{id}", as: {String, String, String, String, Int}
+      date = Time.epoch(date).to_s("%B %d, %Y")
+      view(post)
+    end
+
+    def self.list
+      posts = Emerald::DB.db.query_all("SELECT * FROM posts ORDER BY id", as: Post)
+      view(blog)
+    end
+
+    def self.login()
+      un = "username"
+      pw = "password"
+      pwhash = "hashpw"
+    end
+
+    def self.register()
+      un = "username"
+      pw = "password"
+      pwcf = "passwordconfirm"
+    end
+  end
+end
